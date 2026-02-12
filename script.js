f08d5d2c3d026f5720dfc68c1d0184e91d3de005
goprakash <gopikaprak0504@gmail.com>
2026-02-12 11:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+let seconds = 0;
+let timer;
+const workTime = 25;
+const breakTime = 5;
+let minutes = workTime;
+let m;
+let isRunning = false;
+
+const minuteDisplay = document.getElementById("minutes");
+const secondDisplay = document.getElementById("seconds");
+
+function updateDisplay() {
+    minuteDisplay.textContent = minutes < 10 ? '0' + minutes : minutes;
+    secondDisplay.textContent = seconds < 10 ? '0' + seconds : seconds;
+}
+
+function startTimer() {
+
+    if (isRunning) return;
+
+    isRunning = true;
+
+    m = minutes;
+
+    timer = setInterval(() => {
+        if (minutes === 0 && seconds === 0) {
+            clearInterval(timer);
+            isRunning = false;
+            alert("Time's UP!");
+            switchTime();
+            startTimer();
+            return;
+        }
+
+        seconds--;
+
+        if (seconds < 0) {
+            minutes--;
+            seconds = 59;
+        }
+
+
+        updateDisplay();
+    }, 1000);
+}
+
+function pauseTimer() {
+    clearInterval(timer);
+    isRunning = false;
+}
+
+function resetTime() {
+    clearInterval(timer);
+    isRunning = false;
+    minutes = workTime;
+    seconds = 0;
+    updateDisplay();
+}
+
+// Initial display update
+updateDisplay();
+
+function switchTime() {
+    if (m == workTime) {
+        minutes = breakTime;
+    }
+    else {
+        minutes = workTime;
+    }
+}
